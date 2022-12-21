dc5ad7591d8f524856c1f5faf21222188d8c3417
Moheud Malic <mohidalamsohail22@live.com>
2022-12-21 11:33:02 +0300

    check if nested brackets are accurate

---
+++ Easy/valid_nested_parenthesis.js
@@ -0,0 +1,59 @@
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+ 
+
+// Example 1:
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+// Input: s = "([]){}"
+// Output: true
+
+// Example 3:
+// Input: s = "(]"
+// Output: false
+
+// ---------------------- My Solution ----------------------------------
+
+
+const oppDict = {
+    "]" : "[",
+    "}" : "{",
+    ")" : "("
+}
+
+
+
+var isValid = function(s) {
+    var bracketList = []
+   
+    for (let i=0; i < s.length; i++) {
+        if (s[i] === "{" || s[i] === "(" || s[i] === "[") {
+            bracketList.push(s[i])
+        }
+        else {
+           if (bracketList[bracketList.length-1] === oppDict[s[i]]) {
+               bracketList.pop()
+           }
+           else {
+               return false
+           }
+        }
+    }
+
+    
+    if (bracketList.length == 0) {
+        return true
+    }
+    else 
+    {
+        return false
+    }
+
+};
