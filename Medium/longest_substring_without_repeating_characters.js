6660b8e1647285c3ba8239e3901c5d146341505f
Moheud Malic <mohidalamsohail22@live.com>
2023-01-05 12:38:00 +0300

    Solution for longest substring without repeating chars

--- Medium/longest_substring_without_repeating_characters.js
+++ Medium/longest_substring_without_repeating_characters.js
@@ -19,3 +19,24 @@
 
 // ---------------------- My Solution ----------------------------------
 
+
+var lengthOfLongestSubstring = function(s) {
+    let uniqueChar = []
+    let length = 0
+    
+    for (let i=0; i<s.length; i++) {
+        for (let k=0; k<s.length; k++) {
+             while (uniqueChar.includes(s[k])) {
+                  var index = uniqueChar.indexOf(s[i]);
+                  uniqueChar.splice(index, 1);
+                  i++; 
+             }
+             uniqueChar.push(s[k]);
+             if (length < uniqueChar.length) {
+                 length = uniqueChar.length
+             }
+        }
+       break;
+    } 
+    return length
+};
