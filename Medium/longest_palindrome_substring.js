4f491ecdb1c62cbd6b09797efed91c8e14d02c30
Moheud Malic <mohidalamsohail22@live.com>
2023-01-07 18:59:17 +0300

    find the longest palindrome substring in a string brute force solution

--- Medium/longest_palindrome_substring.js
+++ Medium/longest_palindrome_substring.js
@@ -9,20 +9,59 @@
 // Input: s = "cbbd"
 // Output: "bb"
  
-// ---------------------- My Solution ----------------------------------
+// ---------------------- My Solution BruteForce O(N^3) Time Complexity ----------------------------------
 
-var longestPalindrome = function(s) {
-    for (let i = 0; i<s.length; i++) {
-        for (let k = i+lPalindrome.length; k < s.length; k++) {
-            let substr = s.substring(i, k+1)
-            if (substr == substr.split("").reverse().join("")) {
-                if (lPalindrome.length < substr.length) {
-                    lPalindrome = substr
-                } 
-            }
+// var longestPalindrome = function(s) {
+//      let lPalindrome = ""
 
-        }
+//     for (let i = 0; i<s.length; i++) {
+//         for (let k = i+lPalindrome.length; k < s.length; k++) {
+//             let substr = s.substring(i, k+1)
+//             if (substr == substr.split("").reverse().join("")) {
+//                 if (lPalindrome.length < substr.length) {
+//                     lPalindrome = substr
+//                 } 
+//             }
+
+//         }
+//     }
+//     return lPalindrome
+// }
+
+// ---------------------- My Solution O(N^2) Time Complexity ----------------------------------
+
+
+function expandFromCenter(left, right, string) {
+
+    if (left > right || string == "") {
+        return 0
+    }  
+
+    while((left >= 0 && right < string.length) && string[left] === string[right]) {
+        left--; 
+        right++; 
     }
 
-    return lPalindrome
-}
+    return (right - left - 1)
+} 
+
+var longestPalindrome = function(s) {
+    let lPalindrome = ""
+      let Olength = 0
+      let start = 0
+      let end = 0
+      for (let i=0; i < s.length; i++) {
+         let lengthOdd = expandFromCenter(i,i,s)
+         let lengthEven = expandFromCenter(i,i+1,s)
+
+         let length = Math.max(lengthOdd, lengthEven)
+         if (length > Olength) {
+             Olength = length
+             start = i-(length/2-1)
+             end = i+(length/2)
+         }
+      } 
+
+         return s.substring(start, end+1)
+
+};
