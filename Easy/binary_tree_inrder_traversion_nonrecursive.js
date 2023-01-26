7d5fd01eae78ede86d52343412b9f0d40f0cf11b
Moheud Malic <mohidalamsohail22@live.com>
2023-01-26 12:23:47 +0300

    Traverse a binary tree inorder without using recursion

---
+++ Easy/binary_tree_inrder_traversion_nonrecursive.js
@@ -0,0 +1,67 @@
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+// Example 1:
+// Input: root = [1,null,2,3]
+        //   1
+        //  n  2 
+        //    3 n
+// Output: [1,3,2]
+
+// Example 2:
+// Input: root = []
+// Output: []
+
+// Example 3:
+// Input: root = [1]
+// Output: [1]
+ 
+
+// Constraints:
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+ 
+
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+// ---------------------- My Solution ----------------------------------
+// Morris Algorithm to traverse a binary tree without recursion
+// You can find the explanation for the Morris Algorithm Here: https://www.youtube.com/watch?v=0ydDMtuvbUY
+
+var inorderTraversal = function(root) {
+
+    let tourist = root 
+    let resultArray = []
+    
+    while (tourist != null) {
+       let guide = tourist.left
+    
+       if (tourist.left != null) {
+            while (guide.right != null && guide.right != tourist){
+                guide = guide.right
+            } 
+            
+            if (guide.right == null) {
+                guide.right = tourist
+                tourist = tourist.left
+            }
+            else {
+                guide.right = null
+                resultArray.push(tourist.val)
+                tourist = tourist.right 
+            }
+    
+       }
+       else 
+       {
+           resultArray.push(tourist.val)
+           tourist = tourist.right
+       }
+    
+    }
+    
+    return resultArray
+     
+    };
+
+
