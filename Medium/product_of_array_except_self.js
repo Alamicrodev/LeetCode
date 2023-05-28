9ede5daf4c4de83e631c251dd9e29219431818f7
Moheud Malic <mohidalamsohail22@live.com>
2023-05-28 22:03:43 +0300

    product of all elements of array except self

---
+++ Medium/product_of_array_except_self.js
@@ -0,0 +1,51 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+// Example 1:
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Example 2:
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
+
+// Constraints:
+// 2 <= nums.length <= 105
+// -30 <= nums[i] <= 30
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ 
+// ---------------------- My Solution (O(n)) ----------------------------------
+
+var productExceptSelf = function(nums) {
+    
+    let prefix = []
+    let postfix = []
+ 
+    for (let i = 0; i< nums.length; i++) {
+        if (i==0) {
+            prefix[i] = 1; 
+            continue;
+        }
+  
+        prefix[i] = prefix[i-1]*nums[i-1]
+    }
+ 
+    for (let i = nums.length-1; i>=0; i--) {
+        if (i == nums.length-1) {
+            postfix[i] = 1
+            continue;
+        }
+ 
+        postfix[i] = postfix[i+1]*nums[i+1]
+    }
+ 
+    let results = []
+ 
+    for(let i = 0; i < nums.length; i++) {
+        results[i] = prefix[i]*postfix[i]
+    }
+ 
+ 
+     return results
+ };
